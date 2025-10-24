33616849d88bc54bd83b18363e41cfc9f9803998
Shivam Shinde <shivamshinde92722@gmail.com>
2025-10-24 01:49:58 -0400

    learned conditional rendering in react

--- Project7-props-practice/src/components/joke.jsx
+++ Project7-props-practice/src/components/joke.jsx
@@ -1,13 +1,36 @@
 
 import "../css/joke.css"
+import { useState } from "react"
 
 export default function JokeComponent(props) {
+
+    const [isShown, setIsShown] = useState(false)
+    const [buttonText, setButtonText] = useState("Show the punchline")
+
+    const togglePunchlineVisibility = () => {
+        setIsShown((prev) => {
+            return !prev
+        })
+    
+    }
+
     return (
         <>
-            {props.setup && <p className="setup">Setup: {props.setup}</p>}
-            <p className="punchline">Punchline: {props.punchline}</p> 
+            <h3>New Joke</h3>
+            {props.setup && <p className="setup">{props.setup}</p>}
+            <p className="punchline">{isShown && props.punchline}</p> 
+            <button onClick={togglePunchlineVisibility}>{isShown ? "Hide" : "Show"} punchline</button>
             <hr />
         </>
+
+        // This format is recommended
+        // <>
+        //     <h3>New Joke</h3>
+        //     {props.setup ? <p className="setup">{props.setup}</p>: null}
+        //     <p className="punchline">{isShown ? props.punchline : null}</p> 
+        //     <button onClick={togglePunchlineVisibility}>{isShown ? "Hide" : "Show"} punchline</button>
+        //     <hr />
+        // </>
     )
 }
 
--- Project8-ChefClaude/src/components/Main.jsx
+++ Project8-ChefClaude/src/components/Main.jsx
@@ -12,9 +12,7 @@ export function Main() {
         )
     )
 
-    const handleAddIngredient = (e) => {
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget)
+    const handleAddIngredient = (formData) => {
         const newIngredient = formData.get('ingredient')
         
         setIngredients((ingredients) => {
@@ -25,10 +23,11 @@ export function Main() {
 
     return (
         <main>
-            <form className="flex flex-col gap-4 mt-10 justify-center items-center md:flex-row md:p-4 md:mt-7" onSubmit={handleAddIngredient}>
+            <form className="flex flex-col gap-4 mt-10 justify-center items-center md:flex-row md:p-4 md:mt-7" action={handleAddIngredient}>
                 <input aria-label="Add Ingredient" type="text" name="ingredient" placeholder="e.g., oregano" className="w-lg p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 bg-white placeholder:text-gray-500" />
                 <button type="submit" className="bg-black text-white px-10 py-2 ml-2 rounded-md hover:bg-[#141413] w-2xs cursor-pointer">+ Add Ingredient</button>
             </form>
+            <h2 className="text-3xl text-center font-fold mt-10"></h2>
             <ul className="flex flex-col gap-4 mt-10 justify-start items-center list-disc">
                 {ingredientsList}
             </ul>
