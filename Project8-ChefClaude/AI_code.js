32011e1132bc844467cc660779b348619a40296a
Shivam Shinde <shivamshinde92722@gmail.com>
2025-10-30 23:59:47 -0400

    feat: enhance recipe suggestion formatting and update recipe display structure

--- Project8-ChefClaude/AI_code.js
+++ Project8-ChefClaude/AI_code.js
@@ -1,7 +1,7 @@
 import { InferenceClient } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. The recipe needs to shown on the webpage. So, Properly format it accordingly. 
 `
 const HF_API_KEY = import.meta.env.VITE_HF_API_KEY
 console.log(`HF API KEY: ${HF_API_KEY}`)
@@ -11,11 +11,10 @@ export default async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
     try {
         const response = await client.chatCompletion({
-            // model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-            model: "mistralai/Mistral-7B-Instruct-v0.3",
+            model: "openai/gpt-oss-20b",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
-                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make! Create three sections namely ingredients, instructions and tips.` },
             ],
             max_tokens: 1024,
         })
--- Project8-ChefClaude/src/components/RecipeList.jsx
+++ Project8-ChefClaude/src/components/RecipeList.jsx
@@ -1,13 +1,14 @@
 
-
+import ReactMarkdown from 'react-markdown'
 
 
 const RecipeList = (props) => {
 
 
     return (
-        <section>
-            {props.recipe}
+        <section className='mt-10 text-center text-2xl w-1/2 m-auto'>
+            {props.recipe ? <h2 className='text-3xl font-bold mb-5'>Try This Recipe!</h2> : null}
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
 }
