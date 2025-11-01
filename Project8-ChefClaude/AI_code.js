9770297ceb36d5bb49fdbca22ebfd3316237ea1d
Shivam Shinde <shivamshinde92722@gmail.com>
2025-11-01 01:47:38 -0400

    fix: update system prompt to clarify recipe formatting for web display

--- Project8-ChefClaude/AI_code.js
+++ Project8-ChefClaude/AI_code.js
@@ -1,7 +1,7 @@
 import { InferenceClient } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. The recipe needs to shown on the webpage. So, Properly format it accordingly. 
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. 
 `
 const HF_API_KEY = import.meta.env.VITE_HF_API_KEY
 console.log(`HF API KEY: ${HF_API_KEY}`)
