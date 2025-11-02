3be631bea5413adef6f87e4e7dc0b8e1c3fabd7d
Shivam Shinde <shivamshinde92722@gmail.com>
2025-11-01 22:58:02 -0400

    completed meme generator app

--- project13-meme-generator/src/components/Main.jsx
+++ project13-meme-generator/src/components/Main.jsx
@@ -1,22 +1,43 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Main() {
 
     const [meme, setMeme] = useState({
         topText: "One does not simplt",
         bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
+        imageUrl: ""
     })
 
+    const [allMemeImages, setAllMemeImages] = useState([])
+
     const handleChange = (event) => {
-        const {value, name} = event.currentTarget
+        const { value, name } = event.currentTarget
         setMeme((prevMeme) => {
             return {
                 ...prevMeme,
-                [name]:value
+                [name]: value
             }
         })
     }
+
+    const getMemeImage = () => {
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        const randomMeme = allMemeImages[randomNumber]
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            imageUrl: randomMeme.url
+        }))
+    }
+
+    useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => {
+            setAllMemeImages(data.data.memes)
+            
+        })
+}, [])
+
 
     return (
         <main>
@@ -42,10 +63,13 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button className="bg-blue-300 p-4 rounded-xl w-1/3 text-xl font-bold">Get a new meme image 🖼</button>
+                <button className="bg-blue-300 p-4 rounded-xl w-1/3 text-xl font-bold" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="flex flex-col mt-7 justify-center items-center">
-                <img src={meme.imageUrl} className="w-1/3 rounded-xl" />
+                <img
+                    src={meme.imageUrl}
+                    className="w-1/3 rounded-xl"
+                />
                 <span className="m-2 text-lg font-bold">{meme.topText}</span>
                 <span className="bottom text-lg font-bold">{meme.bottomText}</span>
             </div>
